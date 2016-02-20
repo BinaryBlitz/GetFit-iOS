a8c4256f4c84e1f4f68753bf7f6db8be759cf85b
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-02-20 13:38:48 +0300

    Clean up Trainig tip cell. Add groups for Views

--- Athlete/TrainingTipTableViewCell.swift
+++ Athlete/TrainingTipTableViewCell.swift
@@ -13,15 +13,7 @@ class TrainingTipTableViewCell: UITableViewCell {
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var tipLabel: UILabel!
   
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
 }
