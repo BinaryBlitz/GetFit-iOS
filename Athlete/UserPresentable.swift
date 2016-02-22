6a73f89efcad5521178258a54abf5c14ff968722
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-02-23 02:59:54 +0300

    Create UserPresentable protocol

---
+++ Athlete/UserPresentable.swift
@@ -0,0 +1,14 @@
+//
+//  UserPresentable.swift
+//  Athlete
+//
+//  Created by Dan Shevlyuk on 23/02/2016.
+//  Copyright © 2016 BinaryBlitz. All rights reserved.
+//
+
+protocol UserPresentable {
+  var name: String { get }
+  var info: String { get }
+  var avatarURL: NSURL? { get }
+  var coverImageURL: NSURL? { get }
+}
