a7b7c1dc68715deeb4a3c8612ed217fb97f4c0af
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-03-05 18:03:48 +0300

    Create Professional cell

--- Athlete/ProfessionalTableViewCell.swift
+++ Athlete/ProfessionalTableViewCell.swift
@@ -2,18 +2,28 @@
 //  ProfessionalTableViewCell.swift
 //  Athlete
 //
-//  Created by Dan Shevlyuk on 27/10/15.
-//  Copyright © 2015 BinaryBlitz. All rights reserved.
+//  Created by Dan Shevlyuk on 05/03/2016.
+//  Copyright © 2016 BinaryBlitz. All rights reserved.
 //
 
 import UIKit
 
 class ProfessionalTableViewCell: UITableViewCell {
 
-  @IBOutlet weak var contentImageView: UIImageView!
+  @IBOutlet weak var bannerImageView: UIImageView!
+  @IBOutlet weak var programsBadge: BadgeView!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var followersLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var avatarImageView: CircleImageView!
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    // Initialization code
+  }
+  
+  func configureWith(trainer: Trainer) {
+    nameLabel.text = "\(trainer.firstName) \(trainer.lastName)"
+    programsBadge.text = "10 programs".uppercaseString
   }
 }
