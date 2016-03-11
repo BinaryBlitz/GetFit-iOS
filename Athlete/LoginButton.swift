e0a45c7b2081e61d287f12b8c0d449b5d220367a
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-03-11 14:32:48 +0300

    Edit appearance

--- Athlete/LoginButton.swift
+++ Athlete/LoginButton.swift
@@ -8,8 +8,6 @@
 
 class LoginButton: UIButton {
   
-  private var overlay: UIView?
-  
   var text: String? {
     didSet {
       setTitle(text, forState: UIControlState.Normal)
@@ -28,14 +26,10 @@ class LoginButton: UIButton {
   
   func basicInit() {
     layer.borderColor = UIColor.primaryYellowColor().CGColor
-    layer.borderWidth = 3
+    layer.borderWidth = 2.4
     layer.cornerRadius = 3
     titleLabel?.font = UIFont.boldSystemFontOfSize(18)
     setTitleColor(UIColor.primaryYellowColor(), forState: UIControlState.Normal)
-    
-    let overlay = UIView(frame: frame)
-    overlay.backgroundColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.6)
-    UIView.addContent(overlay, toView: self)
-    sendSubviewToBack(overlay)
+    backgroundColor = UIColor(r: 0, g: 0, b: 0, alpha: 0.6)
   }
 }
