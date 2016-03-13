8440a07e4f367948b8d718984fda82a1a0fe0747
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-03-13 19:47:52 +0300

    Add 'get code' button

--- Athlete/PhoneLoginTableViewController.swift
+++ Athlete/PhoneLoginTableViewController.swift
@@ -16,11 +16,39 @@ class PhoneLoginTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    showNavigationBar()
+    setUpPhoneNumberTextField()
+    setUpNavigationBarButtons()
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    phoneNumberTextField.becomeFirstResponder()
+  }
+  
+  private func showNavigationBar() {
     UIView.animateWithDuration(0.15) {
       self.navigationController?.navigationBarHidden = false
     }
-    
+  }
+  
+  private func setUpPhoneNumberTextField() {
     phoneNumberTextField.placeholder = "8 926 123-45-67"
     phoneNumberTextField.region = "RU"
+  }
+  
+  private func setUpNavigationBarButtons() {
+    let getCodeButton = UIBarButtonItem(title: "Получить код",
+      style: .Done,
+      target: self,
+      action: "getCodeButtonAction"
+    )
+    
+    navigationItem.rightBarButtonItem = getCodeButton
+  }
+  
+  //MARK: - Actions
+  
+  func getCodeButtonAction() {
+    self.presentAlertWithMessage("yo")
   }
 }
