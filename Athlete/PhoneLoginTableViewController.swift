04552e36ad5a8156acec27af4720d0e2a1110acc
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-03-13 19:41:42 +0300

    Remove useless cell class

--- Athlete/PhoneLoginTableViewController.swift
+++ Athlete/PhoneLoginTableViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import PhoneNumberKit
 
 class PhoneLoginTableViewController: UITableViewController {
 
+  @IBOutlet weak var phoneNumberTextField: PhoneNumberTextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     UIView.animateWithDuration(0.15) {
       self.navigationController?.navigationBarHidden = false
     }
+    
+    phoneNumberTextField.placeholder = "8 926 123-45-67"
+    phoneNumberTextField.region = "RU"
   }
 }
