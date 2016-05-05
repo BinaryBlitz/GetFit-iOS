94612011626061cfdd41de427f5f71228c9f827f
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-05-05 15:52:17 +0300

    Edit training tips controller

--- Athlete/TrainingTipsViewController.swift
+++ Athlete/TrainingTipsViewController.swift
@@ -12,7 +12,7 @@ protocol TrainingTipsDelegate {
   func didDismissViewController()
 }
 
-class TrainingTipsViewController: UITableViewController {
+class TrainingTipsViewController: UIViewController {
 
   var delegate: TrainingTipsDelegate?
   var tips = [
@@ -21,8 +21,14 @@ class TrainingTipsViewController: UITableViewController {
     ("The Farce of the lowcarb diet for athletes", "True, monitoring carb intake is one of the best ways to play around with your weight, I don't dispute that. I do it myself, and it can be a powerful tool for people who need to lose a significant amount of weight. But the everyman athlete has no need to go bonkers cutting out all kinds of carbs just for the sake of it, because that sort of eating behavior is not sustainable for an endurance athlete.")
   ]
   
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 18))
+    headerView.backgroundColor = UIColor.whiteColor()
+    tableView.tableHeaderView = headerView
     
     tableView.rowHeight = UITableViewAutomaticDimension
     tableView.estimatedRowHeight = 100
@@ -34,13 +40,13 @@ class TrainingTipsViewController: UITableViewController {
   }
 }
 
-extension TrainingTipsViewController {
+extension TrainingTipsViewController: UITableViewDataSource {
   
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return tips.count
   }
   
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCellWithIdentifier("tipCell") as? TrainingTipTableViewCell else {
       return UITableViewCell()
     }
