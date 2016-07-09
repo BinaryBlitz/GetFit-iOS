1f7e0ea2338a8c099f72d83408822ee42fac1e24
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-07-10 01:26:25 +0300

    Remove old methods for image updates

--- Athlete/ServerManger+User.swift
+++ Athlete/ServerManger+User.swift
@@ -40,33 +40,4 @@ extension ServerManager {
       return nil
     }
   }
-  
-  func update(imageType: Image, withImage image: UIImage,  completion: ((response: ServerResponse<Bool, ServerError>) -> Void)? = nil) -> Request? {
-    typealias Response = ServerResponse<Bool, ServerError>
-    
-    do {
-      let image = Toucan(image: image).resizeByCropping(imageType.imageSize).image
-      let imageKey = imageType.rawValue.lowercaseString
-      let imageData = [imageKey: (image.base64String ?? NSNull())]
-      let parameters: [String: AnyObject] = ["user": imageData]
-      let request = try patch(ServerRoute.User.path, params: parameters)
-      activityIndicatorVisible = true
-      request.validate().responseData { (response) in
-        self.activityIndicatorVisible = false
-        switch response.result {
-        case .Success(_):
-          completion?(response: Response(value: true))
-        case .Failure(let error):
-          print(error)
-          let serverError = ServerError(error: error)
-          completion?(response: Response(error: serverError))
-        }
-      }
-      
-      return request
-    } catch {
-      completion?(response: Response(error: .Unauthorized))
-      return nil
-    }
-  }
 }
