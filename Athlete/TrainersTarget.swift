346a8e5dbc71cfb515ca44995dbf7503e069d5cb
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-07-13 14:16:58 +0300

    Implement Trainers target.

--- Athlete/TrainersTarget.swift
+++ Athlete/TrainersTarget.swift
@@ -6,8 +6,34 @@ import Toucan
 extension GetFit {
   
   public enum Trainers {
+    case Index(filter: TrainersFilter)
+    case Show(id: Int)
   }
   
 }
 
-//extension GetFit.Trainers: TargetType {
+extension GetFit.Trainers: TargetType {
+  
+  public var path: String {
+    switch self {
+    case .Index(_):
+      return "/trainers"
+    case .Show(let id):
+      return "/trainers/\(id)"
+    }
+  }
+  
+  public var method: Moya.Method {
+    return .GET
+  }
+  
+  public var parameters: [String: AnyObject]? {
+    switch self {
+    case .Index(let filter):
+      return ["category": filter.category.rawValue]
+    case .Show(_):
+      return nil
+    }
+  }
+  
+}
