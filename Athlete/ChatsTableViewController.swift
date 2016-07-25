d966f88a6c0249a0807d0e490cf5c58d25449486
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-07-25 12:33:41 +0300

    Remove comments header

--- Athlete/ChatsTableViewController.swift
+++ Athlete/ChatsTableViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ChatsTableViewController.swift
-//  Athlete
-//
-//  Created by Dan Shevlyuk on 02/03/2016.
-//  Copyright © 2016 BinaryBlitz. All rights reserved.
-//
-
 import UIKit
 
 class ChatsTableViewController: UITableViewController {
