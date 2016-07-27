778b7716e769a25b130c8ea8c330cec56cabe323
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-07-27 16:03:43 +0300

    Fix build phases and merge effects

--- Athlete/Post.swift
+++ Athlete/Post.swift
@@ -48,11 +48,11 @@ class Post: Object, ALSwiftyJSONAble {
       self.likesCount = likesCount
     }
 
-    if let commentsCount = json["comments_count"].int {
+    if let commentsCount = jsonData["comments_count"].int {
       self.commentsCount = commentsCount
     }
 
-    if let likeId = json["like_id"].int {
+    if let likeId = jsonData["like_id"].int {
       self.likeId = likeId
     }
 
