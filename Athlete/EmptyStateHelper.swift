64b0ff4891ada32d8dc940544f82b12a9d43b349
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-08-22 02:53:26 +0300

    Create Empty State helper

---
+++ Athlete/EmptyStateHelper.swift
@@ -0,0 +1,47 @@
+import Foundation
+import PureLayout
+
+class EmptyStateHelper {
+  
+  enum Screen {
+    case News
+    case Trainers
+    case Store
+  }
+  
+  static func backgroundViewFor(screenType: Screen) -> UIView {
+    let view = UIView()
+    let titleLable = UILabel()
+    titleLable.textAlignment = .Center
+    titleLable.textColor = UIColor.graySecondaryColor().colorWithAlphaComponent(0.8)
+    titleLable.font = UIFont.systemFontOfSize(16)
+    titleLable.text = titleFor(screenType)
+    
+    let imageView = UIImageView()
+    imageView.contentMode = .ScaleAspectFit
+    imageView.autoSetDimensionsToSize(CGSize(width: 70, height: 70))
+    imageView.image = UIImage(named: "FolderIcon")
+    
+    let stackView = UIStackView(arrangedSubviews: [imageView, titleLable])
+    stackView.axis = .Vertical
+    stackView.alignment = .Center
+    stackView.distribution = .EqualSpacing
+    stackView.spacing = 6
+    
+    view.addSubview(stackView)
+    stackView.autoCenterInSuperview()
+    
+    return view
+  }
+  
+  private static func titleFor(screenType: Screen) -> String {
+    switch screenType {
+    case .News:
+      return "No news to show"
+    case .Trainers:
+      return "No trainers to show"
+    case .Store:
+      return "No programs to show"
+    }
+  }
+}
