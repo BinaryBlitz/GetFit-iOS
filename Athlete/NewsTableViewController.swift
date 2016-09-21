73d562c8b4a67b75e7a63df1dd5e4e9f4a9e7feb
Dan Shevlyuk <danshevlyuk@icloud.com>
2016-09-21 23:58:15 +0300

    Implement Likes creation

--- Athlete/NewsTableViewController.swift
+++ Athlete/NewsTableViewController.swift
@@ -194,26 +194,34 @@ extension NewsTableViewController: PostTableViewCellDelegate {
       static var request: Cancellable?
     }
     
-    //FIXME: this is just wrong
     SharedRequest.request?.cancel()
-    if let indexPath = tableView.indexPathForCell(cell),
-        post = posts?[indexPath.row] {
-      if cell.likeButton.selected {
-        SharedRequest.request = postsProvider.request(.CreateLike(postId: post.id)) { result in
-          switch result {
-          case .Success(let response):
-            do {
-              try response.filterSuccessfulStatusCodes()
-              print("yay! new like")
-            } catch {
-              cell.liked = false
-            }
-          case .Failure(let error):
-            print(error)
-            //TODO: add likes queue
-          }
+    guard let indexPath = tableView.indexPathForCell(cell), post = posts?[indexPath.row]
+        where cell.likeButton.selected else {
+      return
+    }
+    
+    SharedRequest.request = postsProvider.request(.CreateLike(postId: post.id)) { result in
+      switch result {
+      case .Success(let response):
+        do {
+          try response.filterSuccessfulStatusCodes()
+          print("yay! new like")
+        } catch {
+          cell.liked = false
         }
+      case .Failure(let error):
+        print(error)
+        self.addLikeToUploadQueueFor(post)
       }
+    }
+  }
+  
+  private func addLikeToUploadQueueFor(post: Post) {
+    let realm = try! Realm()
+    try! realm.write {
+      let like = Like()
+      realm.add(like)
+      post.like = like
     }
   }
 }
